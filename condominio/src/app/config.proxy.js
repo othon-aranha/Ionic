439a255922ba8da5c94af6111b2e1e33f620c3ea
Othon Aranha <othonaranha@gmail.com>
2021-11-06 13:40:32 -0300

    retirando pasta obsoleta do projeto

--- condominio/src/app/config.proxy.js
+++
@@ -1,9 +0,0 @@
-lang="javascript"
-const proxy = [
-{
-context: "/api",
-target: "http://localhost:8082",
-pathRewrite: { "^/api": "" }
-}
-];
-module.exports = proxy;
--- condominio/src/app/shared/interfaces/base-crud.ts
+++
@@ -1,9 +0,0 @@
-import { BaseResourceModel } from '../model/base-resource-model';
-
-export declare interface BaseCrud {
-    getAll(): Array<BaseResourceModel>;
-    get(id: string): BaseResourceModel;
-    remove(id: string): void;
-    update(obj: BaseResourceModel): void;
-    insert(obj: BaseResourceModel): BaseResourceModel;
-}
